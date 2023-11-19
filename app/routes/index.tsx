b418f4026f88d2b469c961d0632c753f55834032
Matthew Winfield <matthew.thomas.winfield@gmail.com>
2023-11-19 14:18:24 +0000

    Change color scheme to neutral gray

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,11 +1,4 @@
 import { type MetaFunction } from '@remix-run/node'
-import {
-	Tooltip,
-	TooltipContent,
-	TooltipProvider,
-	TooltipTrigger,
-} from '#app/components/ui/tooltip.tsx'
-import { logos, stars } from './logos/logos.ts'
 
 export const meta: MetaFunction = () => [{ title: 'Epic Notes' }]
 
@@ -16,7 +9,6 @@ export default function Index() {
 				<div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
 					<div className="relative shadow-xl sm:overflow-hidden sm:rounded-2xl">
 						<div className="absolute inset-0">
-							<img className="h-full w-full object-cover" src={stars} alt="" />
 							<div className="absolute inset-0 bg-[color:rgba(30,23,38,0.5)] mix-blend-multiply" />
 						</div>
 						<div className="lg:pt-18 relative px-4 pb-8 pt-8 sm:px-6 sm:pb-14 sm:pt-16 lg:px-8 lg:pb-20">
@@ -52,30 +44,6 @@ export default function Index() {
 								guide file for how to get your project off the ground!
 							</p>
 						</div>
-					</div>
-				</div>
-
-				<div className="mx-auto mt-8 max-w-7xl px-4 py-2 sm:px-6 lg:px-8">
-					<div className="flex flex-wrap justify-center gap-8 rounded-3xl bg-slate-100 py-4 dark:bg-slate-200">
-						<TooltipProvider>
-							{logos.map(img => (
-								<Tooltip key={img.href}>
-									<TooltipTrigger asChild>
-										<a
-											href={img.href}
-											className="flex h-16 w-32 justify-center p-1 grayscale transition hover:grayscale-0 focus:grayscale-0"
-										>
-											<img
-												alt={img.alt}
-												src={img.src}
-												className="object-contain"
-											/>
-										</a>
-									</TooltipTrigger>
-									<TooltipContent>{img.alt}</TooltipContent>
-								</Tooltip>
-							))}
-						</TooltipProvider>
 					</div>
 				</div>
 			</div>
